41f0c818fa8ef455aed62cf7217d929cee7cc9e7
Gorav <goravjindal86@gmail.com>
2025-10-30 17:36:17 +0530

    Raceforge ai home page

---
+++ racing-game/src/ui/Intro.tsx
@@ -0,0 +1,43 @@
+import { Suspense, useEffect, useState } from 'react'
+import { useProgress } from '@react-three/drei'
+
+import type { ReactNode } from 'react'
+
+import { useStore } from '../store'
+import { setupSession } from '../data'
+import { Keys } from './Keys'
+
+export function Intro({ children }: { children: ReactNode }): JSX.Element {
+  const [clicked, setClicked] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const { progress } = useProgress()
+  const [session, set] = useStore((state) => [state.session, state.set])
+
+  useEffect(() => {
+    if (clicked && !loading) set({ ready: true })
+  }, [clicked, loading])
+
+  useEffect(() => {
+    if (progress === 100) setLoading(false)
+  }, [progress])
+
+  useEffect(() => {
+    setupSession(set)
+  }, [])
+
+  return (
+    <>
+      <Suspense fallback={null}>{children}</Suspense>
+      <div className={`fullscreen bg ${loading ? 'loading' : 'loaded'} ${clicked && 'clicked'}`}>
+        <div className="stack">
+          <div className="intro-keys">
+            <Keys style={{ paddingBottom: 20 }} />
+            <a className="start-link" href="#" onClick={() => setClicked(true)}>
+              {loading ? `loading ${progress.toFixed()} %` : 'Click to start'}
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
